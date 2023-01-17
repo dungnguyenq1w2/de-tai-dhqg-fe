740b3797067b0267487b29128862e506edc6eaa3
Duong Hai Pham <duonghaipham2001@gmail.com>
2023-01-17 20:40:14 +0700

    update[sheet]: render sheet to screen

--- pages/sheet/index.js
+++ pages/sheet/index.js
@@ -1,13 +1,37 @@
 import Head from 'next/head'
 import { useContext, useState } from 'react'
+import { read, utils, writeFile } from 'xlsx';
 import CHeader from '../../common/components/CHeader'
 import CFileInput from '../../common/components/CFileInput'
 
 export default function Sheet() {
-	const [file, setFile] = useState(null);
+	const [files, setFiles] = useState([]);
+	const [items, setItems] = useState([]);
+	const [header, setHeader] = useState([]);
 
-	const handleChange = (files) => {
+	const handleChange = files => {
+		setFiles(files);
+	}
 
+	const handleReadSheetClick = () => {
+		readSheet(files[0]);
+	}
+	
+	const readSheet = async file => {
+		const reader = new FileReader();
+
+		reader.onload = async event => {
+			const data = event.target.result;
+			const workbook = read(data, { type: 'binary' });
+			const sheetName = workbook.SheetNames[0];
+			const sheet = workbook.Sheets[sheetName];
+			const json = utils.sheet_to_json(sheet);
+
+			setItems(json);
+			setHeader(Object.keys(json[0]));
+		};
+		
+		reader.readAsBinaryString(file);
 	}
 
 	return (
@@ -21,8 +45,26 @@ export default function Sheet() {
 				<CFileInput
 					accept=".xls, .xlsx, .csv"
 					onFileChange={(files) => handleChange(files)} />
+				<button onClick={handleReadSheetClick}>Read sheet</button>
 			</>
-			<div>Sheet</div>
+			<table className='table'>
+				<thead>
+					<tr>
+						{header.map((item, index) => (
+							<th key={index}>{item}</th>
+						))}
+					</tr>
+				</thead>
+				<tbody>
+					{items.map((item, index) => (
+						<tr key={index}>
+							{Object.values(item).map((value, index) => (
+								<td key={index}>{value}</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</div>
 	)
 }
